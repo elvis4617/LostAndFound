59925d4f517a771b507b9369e4c7c52ca53a49e6
Zhuodong Huang <zhuodong45@gmail.com>
2016-09-23 20:51:25 -0400

    add MongoDB

--- README.md
+++ README.md
@@ -1,14 +1,21 @@
 # LostAndFound
 Web Application using React, Bootstrap, Node.js, and MongoDB, etc.
 
-To start:<br/>
-1. install Node.js https://nodejs.org/en/download/<br/>
-2. Use Nodejs command prompt run below command:<br/>
-   git clone https://github.com/elvis4617/LostAndFound.git<br/>
-   cd LostAndFound<br/>
-   cd client<br/>
-   npm install<br/>
-   cd ../server<br/>
-   npm install<br/>
-3. node src/server.js<br/>
-4. open localhost:3000<br/>
+####First time to start:
+
+1. install Node.js https://nodejs.org/en/download/
+2. install mongoDB https://www.mongodb.com/
+3. open first Nodejs command prompt
+4. git clone https://github.com/elvis4617/LostAndFound.git
+5. cd LostAndFound
+6. cd client
+7. npm install
+8. cd ../server
+9. npm install
+10. open second Nodejs command prompt
+11. cd LostAndFound
+12. mkdir laf-data
+13. mongod --dbpath laf-data
+14. go back to frist Nodejs command prompt
+15. node src/server.js
+16. open localhost:3000
--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,32 @@
+var mongo_express = require('mongo-express/lib/middleware');
+var mongo_express_config = require('mongo-express/config.default.js');
+var bodyParser = require('body-parser');
+var validate = require('express-jsonschema').validate;
+var MongoDB = require('mongodb');
+var MongoClient = MongoDB.MongoClient;
+var ObjectID = MongoDB.ObjectID;
+var url = 'mongodb://localhost:27017/LAF';
 var express = require('express');
 var app = express();
 
-app.use(express.static('../client/build'));
+MongoClient.connect(url, function(err, db) {
+  app.use(bodyParser.text());
+  app.use(bodyParser.json());
+  app.use(express.static('../client/build'));
+  app.use('/mongo_express',mongo_express(mongo_express_config));
 
-var port = 3000;
-app.listen(port, function(){
-  console.log('app listening on port 3000');
+/*-------------important:all function must above this line!!!----------------*/
+  app.use(function(err, req, res, next) {
+    if (err.name === 'JsonSchemaValidation') {
+      res.status(400).end();
+    } else {
+      next(err);
+    }
+  });
+
+  var port = 3000;
+  app.listen(port, function(){
+    console.log('app listening on port 3000');
+  });
+
 });
