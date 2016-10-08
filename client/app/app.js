e75fa20b7f8bbe4124554c766d243060b0eb0c0f
Zhuodong Huang <zhuodong45@gmail.com>
2016-10-07 20:51:50 -0400

    add react router

--- client/app/app.js
+++ client/app/app.js
@@ -1,16 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { IndexRoute, Router, Route, hashHistory } from 'react-router';
 import Header from './components/header.js';
+import Sample from './components/sample.js';
+import Sample2 from './components/sample2.js';
+
+class sample extends React.Component {
+  render() {
+    return (
+      <div>
+        <Sample/>
+      </div>
+    );
+  }
+}
+
+class sample2 extends React.Component {
+  render() {
+    return (
+      <div>
+        <Sample2/>
+      </div>
+    );
+  }
+}
+
+
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Header />
-        <h1>Hello, testing!!!</h1>
+        <Header/>
+        {this.props.children}
       </div>
     )
   }
 }
 
-ReactDOM.render(<App/>,document.getElementById('mybody'));
+ReactDOM.render((
+  <Router history={hashHistory}>
+    <Route path="/" component={App}>
+      {/* Show the Feed at / */}
+      <IndexRoute component={sample} />
+      <Route path="/sample2" component={sample2} />
+    </Route>
+  </Router>
+),document.getElementById('mybody'));
---
+++ client/app/components/sample2.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Sample2 extends React.Component {
+  render() {
+    return (
+    <div>
+      Sample22222222
+    </div>
+    )
+  }
+}
